fd0e4fbd95265be4bfb217e297e5aba698a5e900
michealabaho <michealabaho265@gmail.com>
2019-03-25 10:45:47 +0000

    obtaining the granular level information for each goal i.e. extractng the different unique combinations for a particular indicator whose information exists

--- generate_time_series.py
+++ generate_time_series.py
@@ -7,6 +7,8 @@ import sys
 from tabulate import tabulate
 from datetime import datetime
 import matplotlib.pyplot as plt
+import json
+import pprint
 
 pd.options.display.max_columns=40
 
@@ -14,17 +16,20 @@ class generate_time_series:
     def __init__(self, reshaped_df):
         self.reshaped_df = reshaped_df
         self.sd_time_series_dict = {}
+        self.plot_dir = ''
 
     #returning the unique series description for a country
     def return_list_of_unique_series_des(self):
+        sd_list = set([i for i in self.reshaped_df['SeriesDescription']])
         sd_list = set([i for i in self.reshaped_df['SeriesDescription']])
         return sd_list
 
     #creating a directory for the plots
     def create_directories_per_series_des(self):
-        plot_dir = os.path.abspath(os.path.join(os.path.curdir, 'PLOTS'))
-        if not os.path.exists(plot_dir):
-            os.makedirs(plot_dir)
+        self.plot_dir = os.path.abspath(os.path.join(os.path.curdir, 'PLOTS'))
+        if not os.path.exists(self.plot_dir):
+            os.makedirs(self.plot_dir)
+        return self.plot_dir
 
     def generate_combinations_per_series_des(self, sd_list):
         reshaped_data = self.reshaped_df.iloc[1:, :]
@@ -34,12 +39,10 @@ class generate_time_series:
         years = [range(2000, 2019, 1)]
         columns = np.concatenate((columns[:-19], years), axis=None)
         reshaped_data.columns = columns
-        #combinations_file = open('combinations.txt', 'a')
+        plot_dir = self.create_directories_per_series_des()
+        #combinations_file = open(os.path.join(plot_dir, 'combinations.txt'), 'a')
         total_list_of_combinations = []
         for sd in sd_list:
-            # sd_dir = os.path.join(plot_dir, str(sd))
-            # if not os.path.exists(sd_dir):
-            #     os.makedirs(sd_dir)
             # selecting a series description to perform time series on, selecting the most granular detail #CHECKPOINT 2
             reshaped_data_series = reshaped_data.loc[reshaped_data['SeriesDescription'] == sd]
             years_df = reshaped_data_series.iloc[:, -19:].fillna(0)
@@ -70,17 +73,16 @@ class generate_time_series:
             #     combinations_file.write('{}\n'.format(str(i)))
             # combinations_file.write('\n')
             #
-            #combinations_df = pd.DataFrame({i:combinations})
-            #print('\nAttributes to be considered in the various time series prediction include \n {}'.format(parameter_record_list))
-            #print(tabulate(combinations_df, headers='keys', tablefmt=''))
+            # combinations_df = pd.DataFrame({i:combinations})
+            # print('\nAttributes to be considered in the various time series prediction include \n {}'.format(parameter_record_list))
+            # print(tabulate(combinations_df, headers='keys', tablefmt=''))
 
             i = 0
             sd_time_series_dict = []
             for comb in combinations:
-                comb = sorted(comb, key=lambda x:len(x[0]), reverse=True)
                 time_serie_df = pd.DataFrame()
                 time_serie_df = filled_params_df
-                file_saved = ''.join(j for j in ['{}_'.format(str(i[1])) for i in comb])
+                series_description_name = ''.join(j for j in ['{}_'.format(str(i[1])) for i in comb])
 
                 for pair in comb:
                     col, val = pair
@@ -94,7 +96,8 @@ class generate_time_series:
                     time_serie_df = time_serie_df.stack().groupby(level=1).sum().reset_index().rename(columns={'index': 'Year', 0: 'Value'})
                     time_serie_df['Year'] = time_serie_df['Year'].apply(lambda x: datetime.strptime(x, '%Y').year)
                     time_serie_df = time_serie_df[time_serie_df['Year'] != 2018]
-                    sd_time_series_dict.append(time_serie_df)
+                    self.sd_time_series_dict[series_description_name] = time_serie_df
+                    #sd_time_series_dict.append(time_serie_df)
                     #print(tabulate(time_serie_df, headers='keys', tablefmt='psql'))
                     # plt.plot(range(len(time_serie_df['Year'])), time_serie_df['Value'], label=sd)
                     # plt.xlabel('year')
@@ -109,11 +112,6 @@ class generate_time_series:
                     exc_type, exc_obj, exc_tb = sys.exc_info()
                     print(exc_type, exc_obj, exc_tb.tb_lineno)
 
-            self.sd_time_series_dict[sd] = sd_time_series_dict
-        # for i in self.sd_time_series_dict:
-        #     for j in (self.sd_time_series_dict[i]):
-        #         print(tabulate(j, headers='keys', tablefmt='psql'))
-
 
 if __name__=='__main__':
     data_processed = dp.data_preprocessing('egypt.csv')
@@ -122,7 +120,6 @@ if __name__=='__main__':
         unique_sds_in_country = data_reshaped.return_list_of_unique_series_des()
         print('The number of Unique Series_description in Egypt: {}'.format(len(unique_sds_in_country)))
         data_reshaped.generate_combinations_per_series_des(unique_sds_in_country)
-        f = data_reshaped.sd_time_series_dict
-        for i in f:
-            for j in (f[i]):
-                print(tabulate(j, headers='keys', tablefmt='psql'))
+        # f = data_reshaped.sd_time_series_dict
+        # for j in f:
+        #     print(tabulate(f[j], headers='keys', tablefmt='psql'))
