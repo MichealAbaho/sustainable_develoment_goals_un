a2d187dbfa9c550ec5bf3b0549968fd12524d14e
Micheal Abaho <michealabaho265@gmail.com>
2019-06-17 11:34:21 +0100

    Spped up forecasting using dask with fbprophet

--- ts_forecast.py
+++ ts_forecast.py
@@ -16,133 +16,70 @@ from fbprophet import Prophet
 import json
 import warnings
 import sys
+import os
+import warnings
 
 warnings.filterwarnings("ignore", category=Warning)
+#speeding up our timeseries
+from dask.distributed import Client, LocalCluster
+import dask
+import time
 
-class ts_forecast:
-    def __init__(self, ts_dict):
-        self.ts_dict = ts_dict
+def tS_forecast(dict_):
+    n = 4
+    list_of_country_time_series = []
+    json_dict = {}
+    for sd in dict_:
+        df = dict_[sd]
+        if len(df) == 19:
+            v = df[df['Value'] == 0]
+            #ensure you have atleast 4 values are not null values
+            if (len(v) <= 15):
+                years_to_predict = range(2000, 2031, 1)
+                #change the date column to a date type
+                df['Year'] = df['Year'].astype(str).apply(lambda x: hf.obtain_date(x))
+                #df.set_index('Year', inplace=True)
 
-    def tS_forecast(self):
-        n = 4
-        t = []
-        json_dict = {}
-        with open('sdgs_time_series_fb.json', 'w') as sdgs:
-            with open('./PLOTS/combinations.txt', 'a') as comb_file:
-                for sd in self.ts_dict:
-                    df = self.ts_dict[sd]
-                    if len(df) == 18:
-                        v = df[df['Value'] == 0]
-                        #ensure you have atleast 4 values are not null values
-                        if (len(v) <= 13):
-                            years_to_predict = range(2000, 2031, 1)
-                            #change the date column to a date type
-                            df['Year'] = df['Year'].astype(str).apply(lambda x: hf.obtain_date(x))
-                            #df.set_index('Year', inplace=True)
+                #UNDO ONLY WHEN USING FACEBOOKPROPHET
+                df.columns = ['ds','y2']
+                y = [np.NaN if i==0 else i for i in df.y2]
+                df.drop('y2', inplace=True, axis=1)
+                df['y'] = y
 
-                            #UNDO ONLY WHEN USING FACEBOOKPROPHET
-                            df.columns = ['ds','y2']
-                            y = [np.NaN if i==0 else i for i in df.y2]
-                            df.drop('y2', inplace=True, axis=1)
-                            df['y'] = y
-                            print(tabulate(df, headers='keys', tablefmt='psql'))
+                train, validate = df[:(len(df) - n)], df[(len(df) - n):]
+                actual = validate.y.tolist()
+                #FORECAST
+                try:
+                   #FACEBOOK PROHPET
+                    fb_predict, r_mse, mape = fb(train, validate, years_to_predict)
+                    json_dict['Series_description'] = sd
+                    json_dict['Predicted_period'] = list(years_to_predict)[-13:]
+                    json_dict['FB_prophet'] = list(fb_predict['yhat'])[-13:]
+                    json_dict['RMSE'] = r_mse
+                    json_dict['MAPE'] = mape
+                    json_dict['org_values'] = list(df['y'])
 
-                #             #sd_p_value = exmine_the_stationarity(df, sd, comb_file, send_p_values=False)
-                #
-                #             # UNDO ONLY WHEN TESTING THE NULL HYPOTHESIS OF THE STATIONARITY
-                #             # if sd_p_value > 0.05:
-                #             #     find_d(sd, df)
-                #             #     validity_of_determined_p_d_q(sd, df)
-                #
-                            train, validate = df[:(len(df) - n)], df[(len(df) - n):]
-                            actual = validate.y.tolist()
-                            #FORECAST
-                            try:
-                #                 #ARIMA
-                #                 # arima = ARIMA(train, order=(0, 1, 1))
-                #                 # model_arima = arima.fit(disp=0)
-                #                 # fc, se, conf = model_arima.forecast(8, alpha=0.05)  # 95% conf
-                #                 # #Make as pandas series
-                #                 # fc_series = pd.Series(fc, index=years_to_predict)
-                #                 # lower_series = pd.Series(conf[:, 0], index=years_to_predict)
-                #                 # upper_series = pd.Series(conf[:, 1], index=years_to_predict)
-                #                 # predictions = fc.tolist()[:4]
-                #                 # json_dict['Series_description'] = sd
-                #                 # json_dict['Predicted_period'] = list(years_to_predict)
-                #                 # json_dict['Arima'] = fc.tolist()
-                #                 # json_dict['RMSE'] = prediction_accuracies(predictions, actual)
-                #                 #
-                #                 #
-                #                 # #UNDO TO VISUALIZE HOW PREDICTIONS ARE TRENDING vs EXPECTATIONS
-                #                 # # plt.figure(figsize=(12, 5), dpi=100)
-                #                 # # plt.plot(train, label='training')
-                #                 # # plt.plot(validate, label='actual')
-                #                 # # plt.plot(fc_series, label='forecast')
-                #                 # # plt.fill_between(lower_series.index, lower_series, upper_series,
-                #                 # #                  color='k', alpha=.15)
-                #                 # # plt.title('Forecast vs Actuals')
-                #                 # # plt.legend(loc='upper left', fontsize=8)
-                #                 # # plt.show()
-                #                 #
-                #                 # #BASELINE AUTO-ARIMA
-                #                 # model_autoarima = auto_arima(train,
-                #                 #                              trace=True,
-                #                 #                              test='adf',
-                #                 #                              max_p=3, max_q=3,
-                #                 #                              m=1,
-                #                 #                              d = None,
-                #                 #                              seasonal=False,
-                #                 #                              error_action='ignore',
-                #                 #                              suppress_warnings=True,
-                #                 #                              stepwise=True)
-                #                 # a_arima = model_autoarima.fit(train)
-                #                 # arima_forecast = a_arima.predict(n_periods=len(years_to_predict))  # forecasting the next 4 years
-                #                 # predictions = arima_forecast.tolist()[:4]
-                #                 # json_dict['Series_description'] = sd
-                #                 # json_dict['Predicted_period'] = list(years_to_predict)
-                #                 # json_dict['Auto_ARIMA'] = arima_forecast.tolist()
-                #                 # json_dict['RMSE'] = prediction_accuracies(predictions, actual)
-                #                 #
-                #                 #
-                                #FACEBOOK PROHPET
-                                fb = Prophet(weekly_seasonality=True)
-                                fb_model = fb.fit(train)
-                                future = fb_model.make_future_dataframe(periods=len(years_to_predict))
-                                fb_predict = fb.predict(future)
-                                predictions = list(fb_predict['yhat'])[:18][-4:]
-                                actual = validate.y.tolist()
-                                json_dict['Series_description'] = sd
-                                json_dict['Predicted_period'] = list(years_to_predict)[-13:]
-                                json_dict['FB_prophet'] = list(fb_predict['yhat'])[-13:]
-                                r_mse, mape = prediction_accuracies(predictions[:4], actual)
-                                json_dict['RMSE'] = r_mse
-                                json_dict['MAPE'] = mape
-                                json_dict['org_values'] = list(df['y'])
-                #
-                #                 #BASELINE ARMA
-                #                 # arma = ARMA(train, order=(0,1))
-                #                 # arma_model = arma.fit(disp=0)
-                #                 # #print('Coefficients: %s' % arma_model.params)
-                #                 # arma_predict = arma_model.predict(start=len(train), end=len(train) + 7, dynamic=False)
-                #                 # predictions = arma_predict.tolist()
-                #                 # json_dict['Series_description'] = sd
-                #                 # json_dict['Predicted_period'] = list(years_to_predict)
-                #                 # json_dict['ARMA'] = predictions
-                #                 # r_mse, mape = prediction_accuracies(predictions[:4], actual)
-                #                 # json_dict['RMSE'] = r_mse
-                #                 # json_dict['MAPE (%)'] = mape
-                #
-                                t.append(json_dict)
-                                json_dict = {}
+                    list_of_country_time_series.append(json_dict)
+                    json_dict = {}
 
-                            except Exception as e:
-                                exc_type, exc_obj, exc_tb = sys.exc_info()
-                                print(exc_type)
-                                print(exc_obj)
-                                print(exc_tb.tb_lineno)
+                except Exception as e:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    print(exc_type)
+                    print(exc_obj)
+                    print(exc_tb.tb_lineno)
 
-                json.dump(t, sdgs, indent=2, sort_keys=True)
-                sdgs.close()
+    return list_of_country_time_series
+
+@dask.delayed
+def fb(train, validate, years_to_predict):
+    fb = Prophet(weekly_seasonality=True)
+    fb_model = fb.fit(train)
+    future = fb_model.make_future_dataframe(periods=len(years_to_predict))
+    fb_predict = fb.predict(future)
+    predictions = list(fb_predict['yhat'])[:18][-4:]
+    actual = validate.y.tolist()
+    r_mse, mape = prediction_accuracies(predictions[:4], actual)
+    return fb_predict, r_mse, mape
 
 
 
@@ -232,46 +169,39 @@ def prediction_accuracies(prediction, actual):
 
 
 if __name__=='__main__':
-    data_processed = dp.data_preprocessing('egypt.csv')
-    data_reshaped = gts.generate_time_series(data_processed.reshaping())
-    if data_reshaped:
-        unique_sds_in_country = data_reshaped.return_list_of_unique_series_des()
-        print('The number of Unique Series_description in Egypt: {}'.format(len(unique_sds_in_country)))
-        data_reshaped.generate_combinations_per_series_des(unique_sds_in_country)
-        # sub_sub_indicators = data_reshaped.sd_time_series_dict
-        # for i in sub_sub_indicators:
-        #     print(i)
-        #     for j in sub_sub_indicators[i]:
-        #         print(j)
-        #     break
-                #print(tabulate(j, headers='keys', tablefmt='psql'))
-        #output a json containing
-        f = ts_forecast(data_reshaped.sd_time_series_dict)
-        f.tS_forecast()
-        # for i in f.ts_dict:
-        #     for j in (f.ts_dict[i]):
-        #         v = j[j['Value'] == 0]
-        #         if(len(v) < 3):
-        #             print(tabulate(j, headers='keys', tablefmt='psql'))
+    cluster = LocalCluster(n_workers=5, scheduler_port=0, diagnostics_port=0, processes=False)
+    client = Client(cluster)
+    start_time = time.time()
+    t = []
+    print("Started ###########################################################")
+    #data_processed = dp.data_preprocessing('allcountries.csv').reshaping()
+    clean_data = dask.delayed(dp.data_preprocessing)('allcountries.csv')
+    data_processed = dask.delayed(clean_data.reshaping())
+    data_processed = dask.delayed(data_processed).compute()
 
+    for country in data_processed:
+        country_dir = hf.create_directories_per_series_des(name=country)
+        with open(os.path.join(country_dir, 'sdgs_time_series_fb.json'), 'w') as sdgs:
+            with open(os.path.join(country_dir, 'combinations.txt'), 'w') as comb_file:
+                data_reshaped = gts.generate_time_series(data_processed[country])
+                if data_reshaped:
+                    unique_sds_in_country = data_reshaped.return_list_of_unique_series_des()
+                    print('The number of Unique Series_description in {}: {}'.format(country, len(unique_sds_in_country)))
+                    country_time_series, combinations = data_reshaped.generate_combinations_per_series_des(unique_sds_in_country)
 
-        #baseline set of models for time series forecasting
-                        # models = {'AR': AR(train), 'ARMA': ARMA(train, order=(0, 1))}
-                        # for i in (models):
-                        #     mod = models[i].fit()
-                        #     print('MODEL : {}'.format((i)))
-                        #     print('Coefficients: %s' % mod.params)
-                        #     predictions = mod.predict(start=len(train), end=len(train) + len(validate)-1, dynamic=False)
-                        #     for j in range(len(predictions)):
-                        #         print('Predicted - {:.4f} yet Actual Value is {:.4f}'.format(predictions[j], validate_bench[j]))
-                        #     print('RMSE is {}\n'.format(math.sqrt(mean_squared_error(validate_bench, predictions))))
+                    country_time_series_list = tS_forecast(country_time_series)
 
+                    json.dump(country_time_series_list, sdgs, indent=2, sort_keys=True)
 
-                        # print('\nMODEL: ARIMA')
-                        # for j in range(len(arima_predict)):
-                        #     print('Predicted - {:.4f} yet Actual Value is {:.4f}'.format(arima_predict[j], validate_bench[j]))
-                        # auto_arima_df = pd.DataFrame(forecast, columns=['Prediction'])
-                        # print('RMSE is {}'.format(math.sqrt(mean_squared_error(validate_bench, arima_predict))))
-                        # print('MAE is ', mean_absolute_error(validate_bench, arima_predict))
-                        # for x, y in zip(years_to_predict, forecast[-13:]):
-                        #     print('Prediction for {} is {}'.format(x, y))
+                    comb_file.write('{}\n'.format(country))
+                    for i in combinations:
+                        comb_file.write('{}\n'.format(str(i)))
+                    comb_file.write('\n')
+
+                comb_file.close()
+            sdgs.close()
+
+    print('{} Complete'.format(country))
+    end_time = time.time()
+    print('EXECUTION TIME ############################################################# \n{}'.format((end_time-start_time)))
+
